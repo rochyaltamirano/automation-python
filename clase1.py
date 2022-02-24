bb9bf7db94169fc8c2ed2c9d5ea1b14258e9fb70
rochyaltamirano <rochyaltamirano@gmail.com>
2022-02-24 15:41:37 -0300

    Guarda elemento en una variable para usarla luego

--- clase1.py
+++ clase1.py
@@ -1,3 +1,4 @@
+from re import search
 from selenium import webdriver
 import unittest
 
@@ -5,8 +6,11 @@ tc = unittest.TestCase('__init__')
 driver = webdriver.Chrome('/Users/rocioaltamirano/projects/automation/clase1/chromedriver')
 driver.get('http://automationpractice.com/index.php')
 
-driver.find_element_by_id('search_query_top').send_keys('hola')
-driver.find_element_by_name('submit_search').click()
+search_box = driver.find_element_by_id('search_query_top')
+search_box.send_keys('hola')
+
+search_button = driver.find_element_by_name('submit_search')
+search_button.click()
 
 tc.assertEqual('No results were found for your search "hola"', driver.find_element_by_xpath('//*[@id="center_column"]/p').text)
 
