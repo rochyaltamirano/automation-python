5f9dec8384ae2a5f61ddc0b5f5b11b5f93d09ed8
rochyaltamirano <rochyaltamirano@gmail.com>
2022-02-24 22:04:13 -0300

    Ejemplo: Buscar por link/partial link

--- clase1.py
+++ clase1.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 import unittest
 
 tc = unittest.TestCase('__init__')
-driver = webdriver.Chrome('/Users/rocioaltamirano/projects/automation/clase1/chromedriver')
+driver = webdriver.Chrome('/Users/rocioaltamirano/automation-python/chromedriver')
 driver.get('http://automationpractice.com/index.php')
 
 search_box = driver.find_element_by_id('search_query_top')
@@ -12,7 +12,18 @@ search_box.send_keys('hola')
 search_button = driver.find_element_by_name('submit_search')
 search_button.click()
 
-tc.assertEqual('No results were found for your search "hola"', driver.find_element_by_xpath('//*[@id="center_column"]/p').text)
+results_label = driver.find_element_by_xpath('//*[@id="center_column"]/p')
+
+tc.assertEqual('No results were found for your search "hola"', results_label.text)
+
+#
+
+women_link = driver.find_element_by_link_text('Women')
+women_link.click()
+
+
+dress_link = driver.find_element_by_partial_link_text('Dres')
+dress_link.click()
 
 driver.close()
 driver.quit()
